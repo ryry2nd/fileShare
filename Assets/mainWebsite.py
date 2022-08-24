7c107c4a258da1e1ce82bb4ef418d0b94b9cacd4
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-08-24 08:38:09 -0400

    bug fixes

--- Assets/mainWebsite.py
+++ Assets/mainWebsite.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, redirect, url_for, send_file, flash
 import html, os
 
 class MainWebsite:
     def __init__(self, app:Flask):
         self.app = app
-        self.initPoll(app)
+        self.init(app)
 
     def isForm(self):
         for idx, d in request.form.items():
@@ -12,22 +12,36 @@ class MainWebsite:
                 return True
         return False
 
-    def initPoll(self, app: Flask):
+    def render_share(self):
+        return render_template("fileShare.html", filenames=os.listdir(self.app.config["UPLOAD_FOLDER"]), password=self.app.config["password"])
+
+
+    def init(self, app: Flask):
         @app.route("/", methods=["POST", "GET"])
         def index():
             if request.method == "GET":
                 return render_template("index.html")
             else:
                 if self.isForm():
-                    if html.escape(request.form["password"]) == "Spongebob!":
-                        return render_template("fileShare.html", filenames=os.listdir(self.app.config["UPLOAD_FOLDER"]))
+                    if html.escape(request.form["password"]) == app.config["password"]:
+                        return self.render_share()
                     else:
                         return render_template("index.html")
                 else:
-                    f = request.files['file']
-                    f.save(os.path.join(self.app.config["UPLOAD_FOLDER"], f.filename))
-                    return redirect(url_for('index'))
+                    if 'file' not in request.files:
+                        flash('No file part')
+                        return self.render_share()
 
-        @app.route('/download', methods=["POST"])
+                    file = request.files['file']
+
+                    if file.filename == '':
+                        flash('No file selected for uploading')
+                        return self.render_share()
+
+                    file.save(os.path.join(self.app.config["UPLOAD_FOLDER"], file.filename))
+                    return self.render_share()
+
+        @app.route("/download_file", methods=["POST"])
         def download_file():
-            return send_file(os.path.join(self.app.config["UPLOAD_FOLDER"], request.form["fileName"]), as_attachment=True)
+            if request.form["password"] == app.config["password"]:
+                return send_file(os.path.join(self.app.config["UPLOAD_FOLDER"], request.form["fileName"]), as_attachment=True)
--- main.py
+++ main.py
@@ -7,14 +7,18 @@ HOST = '0.0.0.0'
 PORT = 80
 THREADS = 6
 
+SECRET_KEY = os.urandom(24)
 UPLOAD_FOLDER = 'data'
+PASSWORD = "p"
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 app = Flask(__name__)
 
+app.config['password'] = PASSWORD
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = SECRET_KEY
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
@@ -25,4 +29,5 @@ main = MainWebsite(app)
 
 if __name__ == '__main__':
     print(f"connecting with ip: {IP} and port: {PORT}")
-    serve(app, host=HOST, port=PORT, threads=THREADS)
+    app.run(host=HOST, port=PORT)
+    #serve(app, host=HOST, port=PORT, threads=THREADS)
